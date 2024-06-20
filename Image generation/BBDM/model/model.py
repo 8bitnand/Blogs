e311eb37dcc53d6180529f1e1b847d010c0399ca
8bitnand <8bitnand@gmail.com>
2024-06-20 20:30:25 +0530

    sampling step in progress

--- Image generation/BBDM/model/model.py
+++ Image generation/BBDM/model/model.py
@@ -1,29 +1,117 @@
 from diffusers import UNet2DModel, VQModel
-import torch
 from typing import Union
-from omegaconf import OmegaConf
+from omegaconf import DictConfig
 import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torchvision
 
 
-class BBDM(torch.nn.Module):
-    def __init__(self, configs: Union[dict]) -> None:
+class BBDM(nn.Module):
+    def __init__(self, configs: Union[DictConfig]) -> None:
         super().__init__()
-        self.unet = UNet2DModel(**vars(configs.model.BB.params.UNetParams))
+        self.unet = UNet2DModel(**(configs.model.BB.params.UNetParams))
         self.timesteps = configs.model.BB.params.num_timesteps
         self.max_var = 1
-        self.m_t = torch.sin(torch.linspace(0.01, 0.99, self.timesteps))
-        self.m_tminus = torch.cat([torch.tensor([0.0]), self.m_t[:-1]])
+        self.m_t = torch.sin(torch.linspace(0.01, 0.99, self.timesteps + 1))
         self.variance = 2.0 * (self.m_t - self.m_t**2) * self.max_var
+        self.eta = 1
+        self.loss_type = configs.model.BB.params.loss_type
 
     def forward(self, timestep, x, y):
         # add noise & y to x - (1-mt)*x + mt*y + sqrt(var)*noise
         noise = torch.rand_like(x)
+        sigma_t = torch.sqrt(self.variance[timestep])
+
         noisy_image = (
-            (1 - self.m_t[timestep]) * x
-            + self.m_t * y
-            + torch.sqrt(self.variance[timestep]) * noise
+            (1 - self.m_t[timestep].view(-1, 1, 1, 1)) * x
+            + self.m_t[timestep].view(-1, 1, 1, 1) * y
+            + sigma_t.view(-1, 1, 1, 1) * noise
+        )
+        nois_added = (
+            self.m_t[timestep].view(-1, 1, 1, 1) * (y - x)
+            + sigma_t.view(-1, 1, 1, 1) * noise
         )
 
         # predict noise and y
+        pred_noise = self.unet(
+            noisy_image,
+            timestep,
+        )["sample"]
 
         # calculate loss
+        if self.loss_type == "l1":
+            recloss = (nois_added - pred_noise).abs().mean()
+        elif self.loss_type == "l2":
+            recloss = F.mse_loss(nois_added, pred_noise)
+        else:
+            raise NotImplementedError()
+
+        return recloss
+
+    def predict_x0_from_objective(self, timestep, x, y, objective_recon):
+        """
+        predict initial image from current timestep
+        image at time t - dest image added - predected noise
+        xt - y*m_t - sqrt(var)* pred_noise
+        """
+
+        x0_recon = (
+            x
+            - self.m_t[timestep].view(-1, 1, 1, 1) * y
+            - torch.sqrt(self.variance[timestep]).view(-1, 1, 1, 1) * objective_recon
+        ) / (1.0 - self.m_t[timestep].view(-1, 1, 1, 1))
+
+        return x0_recon
+
+    @torch.no_grad
+    def sample(self, cond):
+        img = cond
+        for ts in range(self.timesteps):
+            t = torch.full((img.shape[0],), ts, device=img.device, dtype=torch.long)
+            n_t = torch.full(
+                (img.shape[0],),
+                ts + 1,
+                device=img.device,
+                dtype=torch.long,
+            )
+
+            # img0 is not used
+            img_rec = self.unet(img, ts)["sample"]
+            img0 = self.predict_x0_from_objective(t, img, cond, img_rec)
+
+            m_t = self.m_t[t].view(-1, 1, 1, 1)
+            m_nt = self.m_t[t + 1].view(-1, 1, 1, 1)
+            var_t = self.variance[t].view(-1, 1, 1, 1)
+            var_nt = self.variance[n_t].view(-1, 1, 1, 1)
+            sigma2_t = (
+                (var_t - var_nt * (1.0 - m_t) ** 2 / (1.0 - m_nt) ** 2) * var_nt / var_t
+            )
+            sigma_t = torch.sqrt(sigma2_t) * self.eta
+            noise = torch.randn_like(img)
+            x_tminus_mean = (
+                (1.0 - m_nt) * img_rec
+                + m_nt * cond
+                + torch.sqrt((var_nt - sigma2_t) / var_t)
+                * (img - (1.0 - m_t) * img_rec - m_t * cond)
+            )
+
+            print(var_t)
+            img = x_tminus_mean + sigma_t * noise
+            # print(img)
+
+        return img
+
+
+class LBBDM(BBDM):
+    def __init__(self, configs: DictConfig) -> None:
+        super().__init__(configs)
+        self.configs = configs
+        self.vqmodel = VQModel(**configs.model.VQGAN.params)
+
+    def forward(self, timestep, x, y):
+        x_latent = self.vqmodel.encode(x)["latents"]
+        y_latent = self.vqmodel.encode(y)["latents"]
+
+        return super().forward(timestep, x_latent.detach(), y_latent.detach())
--- Image generation/BBDM/train.py
+++ Image generation/BBDM/train.py
@@ -1,14 +1,43 @@
 from omegaconf import OmegaConf
 from typing import Union, Optional
-from model.model import BBDM
-import click 
+from model.model import BBDM, LBBDM
+import click
+import torch
+import PIL.Image as I
+from torchvision.transforms import v2
+import numpy as np
+
+
+
+def extract(a, t, x_shape):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+
+
+
 
 @click.command()
 @click.option("--config", "-c", help="Path to the config file")
 def main(config):
 
     configs = OmegaConf.load(config)
-    
+    # t = v2.Compose(
+    #     [
+    #         v2.PILToTensor(),
+    #         v2.Resize(size=(128,128), antialias=True),
+    #         v2.ToDtype(torch.float32),
+    #     ]
+    # )
     bbmodel = BBDM(configs)
+    # x = I.open("model/resources/test_image.jpg")
+    # x = torch.unsqueeze(t(x), dim=0)
+    x = torch.randn((2, 3, 128, 128))
+    # bbmodel(torch.tensor([1,1], dtype=torch.int32), x, x)
+    bbmodel.sample(x)
+    # l = LBBDM(configs)
+    # print(l(torch.tensor([1,1], dtype=torch.int32), x, x))
+    
+
 
 main()
